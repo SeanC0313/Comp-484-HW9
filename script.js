0a3591b07efdce879348e4c063050b33562ba33c
SeanC0313 <sean-anthony.cano.622@my.csun.edu>
2026-04-01 22:58:00 -0700

    Part 4 Done

--- script.js
+++ script.js
@@ -43,6 +43,14 @@ const isInteger2 = Number.isInteger(c2);
 const isInteger3 = Number.isInteger(c3);
 const isInteger4 = Number.isInteger(c4);
 
+// Conditional for invalid number
+let m3 = "";
+if (isNaN3) {
+    m3 = "This is NOT a valid number.";
+} else {
+    m3 = "This is a valid number.";
+}
+
 // Create result strings
 const r1 = "Original: \"" + v1 + "\" | Converted: " + c1 + " | isNaN: " + isNaN1 + " | isInteger: " + isInteger1;
 const r2 = "Original: \"" + v2 + "\" | Converted: " + c2 + " | isNaN: " + isNaN2 + " | isInteger: " + isInteger2;
@@ -50,7 +58,7 @@ const r3 = "Original: \"" + v3 + "\" | Converted: " + c3 + " | isNaN: " + isNaN3
 const r4 = "Original: \"" + v4 + "\" | Converted: " + c4 + " | isNaN: " + isNaN4 + " | isInteger: " + isInteger4;
 
 // Display results
-const conversionOutput = "<p>" + r1 + "</p><p>" + r2 + "</p><p>" + r3 + "</p><p>" + r4 + "</p>";
+const conversionOutput = "<p>" + r1 + "</p><p>" + r2 + "</p><p>" + r3 + "<br>" + m3 + "</p><p>" + r4 + "</p>";
 document.getElementById("numberConversionOutput").innerHTML = conversionOutput;
 
 // ==========================
@@ -72,11 +80,21 @@ const formattedSubtotal = subtotal.toFixed(2);
 const formattedTax = taxAmount.toFixed(2);
 const formattedTotal = total.toFixed(2);
 
+// Message
+let totalMessage = "";
+
+if(total > 30) {
+    totalMessage = "Total is greater than $30.";
+} else {
+    totalMessage = "Total is $30 or less.";
+}
+
 // Display
 const mathOutput = "<p>Item price: $" + item.toFixed(2) + "</p>" +
                    "<p>Shipping: $" + shipping.toFixed(2) + "</p>" +
                    "<p>Tax: " + (tax * 100).toFixed(0) + "%</p>" +
                    "<p>Subtotal: $" + formattedSubtotal + "</p>" +
                    "<p>Tax Amount: $" + formattedTax + "</p>" +
-                   "<p>Total: $" + formattedTotal + "</p>";
+                   "<p>Total: $" + formattedTotal + "</p>" +
+                   "<p>" + totalMessage + "</p>";
 document.getElementById("mathOutput").innerHTML = mathOutput;
